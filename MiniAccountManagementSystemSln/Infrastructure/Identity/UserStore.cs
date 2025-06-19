627278235711db07879d492e35652476b17e7ed2
itsonlytamim <your.tamim@gmail.com>
2025-06-19 23:40:01 +0600

    UserManagement Identity

---
+++ MiniAccountManagementSystemSln/Infrastructure/Identity/UserStore.cs
@@ -0,0 +1,112 @@
+﻿using Core.Entities;
+using Dapper;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity
+{
+    public class UserStore : IUserStore<ApplicationUser>
+    {
+        private readonly DbContext _context;
+
+        public UserStore(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IdentityResult> CreateAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync("sp_User_Create", new
+                {
+                    user.Id,
+                    user.UserName,
+                    user.NormalizedUserName,
+                    user.Email,
+                    user.NormalizedEmail,
+                    user.EmailConfirmed,
+                    user.PasswordHash,
+                    user.SecurityStamp,
+                    user.ConcurrencyStamp,
+                    user.PhoneNumber,
+                    user.PhoneNumberConfirmed,
+                    user.TwoFactorEnabled,
+                    user.LockoutEnd,
+                    user.LockoutEnabled,
+                    user.AccessFailedCount
+                }, commandType: CommandType.StoredProcedure);
+            }
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> UpdateAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync("sp_User_Update", new
+                {
+                    user.Id,
+                    user.UserName,
+                    user.NormalizedUserName,
+                    user.Email,
+                    user.NormalizedEmail,
+                    user.EmailConfirmed,
+                    user.PasswordHash,
+                    user.SecurityStamp,
+                    user.ConcurrencyStamp,
+                    user.PhoneNumber,
+                    user.PhoneNumberConfirmed,
+                    user.TwoFactorEnabled,
+                    user.LockoutEnd,
+                    user.LockoutEnabled,
+                    user.AccessFailedCount
+                }, commandType: CommandType.StoredProcedure);
+            }
+            return IdentityResult.Success;
+        }
+
+        public async Task<IdentityResult> DeleteAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync("sp_User_Delete", new { user.Id }, commandType: CommandType.StoredProcedure);
+            }
+            return IdentityResult.Success;
+        }
+
+        public async Task<ApplicationUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            using (var connection = _context.CreateConnection())
+            {
+                return await connection.QuerySingleOrDefaultAsync<ApplicationUser>("sp_User_FindById", new { Id = userId }, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public async Task<ApplicationUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            using (var connection = _context.CreateConnection())
+            {
+                return await connection.QuerySingleOrDefaultAsync<ApplicationUser>("sp_User_FindByName", new { NormalizedUserName = normalizedUserName }, commandType: CommandType.StoredProcedure);
+            }
+        }
+        public Task<string> GetUserIdAsync(ApplicationUser user, CancellationToken cancellationToken) => Task.FromResult(user.Id);
+        public Task<string> GetUserNameAsync(ApplicationUser user, CancellationToken cancellationToken) => Task.FromResult(user.UserName);
+        public Task SetUserNameAsync(ApplicationUser user, string userName, CancellationToken cancellationToken) { user.UserName = userName; return Task.CompletedTask; }
+        public Task<string> GetNormalizedUserNameAsync(ApplicationUser user, CancellationToken cancellationToken) => Task.FromResult(user.NormalizedUserName);
+        public Task SetNormalizedUserNameAsync(ApplicationUser user, string normalizedName, CancellationToken cancellationToken) { user.NormalizedUserName = normalizedName; return Task.CompletedTask; }
+        public void Dispose() { }
+    }
+
+}
