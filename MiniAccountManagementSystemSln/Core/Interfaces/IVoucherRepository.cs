cc902e5ee3a8570681ef3b44a77bdd3634d8266b
itsonlytamim <your.tamim@gmail.com>
2025-06-19 21:42:30 +0600

    Voucher Repo

---
+++ MiniAccountManagementSystemSln/Core/Interfaces/IVoucherRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IVoucherRepository
+    {
+        Task<int> CreateAsync(Voucher voucher);
+        Task<Voucher?> GetByIdAsync(int voucherId);
+        Task<IEnumerable<Voucher>> GetAllAsync(int pageNumber, int pageSize);
+        Task<IEnumerable<Voucher>> GetByTypeAsync(VoucherType voucherType);
+    }
+}
