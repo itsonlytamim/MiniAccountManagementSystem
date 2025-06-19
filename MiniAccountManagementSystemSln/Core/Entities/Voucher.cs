03d88f333fc8fce706fb3f4ab107f4d7044c029f
itsonlytamim <your.tamim@gmail.com>
2025-06-19 21:39:52 +0600

    Voucher Entity

---
+++ MiniAccountManagementSystemSln/Core/Entities/Voucher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Voucher
+    {
+        public int VoucherId { get; set; }
+        public DateTime VoucherDate { get; set; }
+        public VoucherType VoucherType { get; set; }
+        public string? ReferenceNo { get; set; }
+        public string? Narration { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<VoucherDetail> Details { get; set; } = new List<VoucherDetail>();
+    }
+}
