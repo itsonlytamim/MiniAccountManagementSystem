c8c424aff6e41f8d947a2ff486f6465a54e1921f
itsonlytamim <your.tamim@gmail.com>
2025-06-19 21:32:56 +0600

    Account Interface

---
+++ MiniAccountManagementSystemSln/Core/Interfaces/IAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IAccountRepository
+    {
+        Task<IEnumerable<Account>> GetAllHierarchicalAsync();
+        Task<int> CreateAsync(Account account);
+        Task UpdateAsync(Account account);
+        Task DeleteAsync(int accountId);
+        Task<Account?> GetByIdAsync(int accountId);
+    }
+}
